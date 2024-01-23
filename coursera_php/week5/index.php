498120c543c021b234935437326665d71bcff550
glib05 <119256208+glib05@users.noreply.github.com>
2024-01-23 13:26:11 +0200

    add week 5

---
+++ coursera_php/week5/index.php
@@ -0,0 +1,18 @@
+
+<title>ІП-24 Новиков Гліб Костянтинович</title>
+<h1>ІП-24 Новиков Гліб Костянтинович</h1>
+<p><?php
+	print "The SHA256 hash of \"ІП-24 Новиков Гліб Костянтинович\" is ".hash('sha256', 'ІП-24 Новиков Гліб Костянтинович');
+?></p>
+<pre>ASCII ART:
+	********
+	   **
+	   **
+	   **
+	   **
+	   **
+	********
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
